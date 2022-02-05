31b69b2415f6df365dbc029d21c5f5a4e81091da
ashevliakov <shevlyakov.tlkpi@gmail.com>
2022-02-05 23:48:53 +0200

    Add Purchase (#3)
    * Add auth controllers
    
    * Add profile logic and error handling
    
    * Add purchase logic
    
    * Reforfat files

--- back/repositories/mongoRepository.js
+++ back/repositories/mongoRepository.js
@@ -1,12 +1,21 @@
-const mongoose = require('mongoose');
 const Menu = require('../models/menu');
 const User = require('../models/user');
-const {badRequestError, internalServerError} = require('../errors');
+
+const Purchase = require('../models/Purchase');
+const {internalServerError} = require('../errors');
 
 module.exports = {
     async getMenuCollectionBySubject(subject) {
         try {
             return await Menu.find({'subject': subject});
+        } catch(err) {
+            throw new internalServerError(err);
+        }
+    },
+
+    async getMenuById(menuId) {
+        try {
+            return await Menu.findOne({'_id': menuId})
         } catch(err) {
             throw new internalServerError(err);
         }
@@ -23,6 +32,31 @@ module.exports = {
     async findUser(userInfo) {
         try {
             return await User.findOne(userInfo)
+        } catch(err) {
+            throw new internalServerError(err);
+        }
+    },
+
+    async updateUser(userInfo) {
+        try {
+            await User.updateOne({email: userInfo.email} , userInfo)
+        } catch(err) {
+            throw new internalServerError(err);
+        } 
+    },
+
+    async addPurchase(purchase) {
+        try {
+            await Purchase.insertMany(purchase);
+        } catch(err) {
+            throw new internalServerError(err);
+        }
+    },
+
+    async getPurchase(email) {
+        try {
+            return await Purchase.aggregate([{ $match : { email: email } } , 
+                                            {$group : {_id:{menuId:"$menuId", email:"$email" , date: "$date"}, amount:{$sum:1}}}]);
         } catch(err) {
             throw new internalServerError(err);
         }
