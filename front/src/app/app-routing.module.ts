5d1c5abd0ffa0015812a17e39954d1da98bbca0b
alex123411 <57320718+alex123411@users.noreply.github.com>
2022-02-07 05:58:41 +0200

    navbar+carousel

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MenusComponent } from './components/menus/menus.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'menu', component: MenusComponent},
+  { path: 'contacts', component: ContactsComponent},
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenusComponent } from './components/menus/menus.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+//import { CarouselComponent } from './shared/components/carousel/carousel.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    MenusComponent,
+    ContactsComponent,
+    //CarouselComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    SharedModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    //CarouselComponent
+  ]
 })
 export class AppModule { }
